3370aface46d2794eb9b6121af5ca35fc5f2be7d
hosunny <https://lab.ssafy.com/cook8172/hws.git>
2022-08-15 18:17:08 +0900

    0815-1문제

--- 스터디(같이 푸는 문제)/0814/1244. 호선. 스위치 켜고 끄기 30840KB, 시간 72ms/백준 1244.py
+++ 스터디(같이 푸는 문제)/0814/1244. 호선. 스위치 켜고 끄기 30840KB, 시간 72ms/백준 1244.py
@@ -6,19 +6,19 @@ sw_lst = list(map(int, input().split()))
 p = int(input())
 for i in range(p):
     A, B = map(int, input().split())
-    if A == 1:
+    if A == 1:                                  # 성별이 남자일 때
         for j in range(1, N + 1):
-            if j % B == 0:
+            if j % B == 0:                      # 숫자의 배수의 값을 변경
                 if sw_lst[j - 1] == 0:
                     sw_lst[j - 1] = 1
                 else:
                     sw_lst[j - 1] = 0
-                    # [0, 0, 1, 0, 0, 1, 0, 1]
-    else:
-        for k in range(1, (N-1) // 2):
-            if (B - 1) - k >= 0 and (B - 1) + k <= N - 1:
-                if sw_lst[(B - 1) - k] == sw_lst[(B - 1) + k]:
-                    if sw_lst[(B - 1) - k] == 0:
+
+    else:                                       # 성별이 여자일 때
+        for k in range(1, (N-1) // 2):          # 주어진 숫자를 기준으로 양 옆을 조사하므로, 전체의 반만 조사!
+            if (B - 1) - k >= 0 and (B - 1) + k <= N - 1:   # 인덱스 오류를 피하기 위해 범위 조절
+                if sw_lst[(B - 1) - k] == sw_lst[(B - 1) + k]:    # 기준 숫자의 양 옆이 같을 때
+                    if sw_lst[(B - 1) - k] == 0:                  # 양 옆의 값을 변경
                         sw_lst[(B - 1) - k] = 1
                         sw_lst[(B - 1) + k] = 1
                     else:
@@ -26,12 +26,12 @@ for i in range(p):
                         sw_lst[(B - 1) + k] = 0
                 else:
                     break
-        if sw_lst[B - 1] == 0:
+        if sw_lst[B - 1] == 0:     # 기준이 되는 숫자의 값을 변경
             sw_lst[B - 1] = 1
         else:
             sw_lst[B - 1] = 0
 
-for i in range(len(sw_lst)):
+for i in range(len(sw_lst)):       # 20개씩 끊어서 출력
     if i % 10 == 9:
         print(sw_lst[i], end='\n')
     else:
--- 스터디(같이 푸는 문제)/0814/14696. 호선. 딱지놀이 30840KB, 시간 324ms/백준 14696.py
+++ 스터디(같이 푸는 문제)/0814/14696. 호선. 딱지놀이 30840KB, 시간 324ms/백준 14696.py
@@ -2,36 +2,36 @@ n = int(input())
 for i in range(1, n + 1):
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
-    A = a[1:]
-    B = b[1:]
+    A = a[1:]            # 딱지의 그림부분 슬라이싱
+    B = b[1:]            # 딱지의 그림부분 슬라이싱
     A.sort()
     B.sort()
 
     while True :
-        if A == B :
+        if A == B :            # 둘이 같을 경우 무승부
             print('D')
             break
-        if A == [] :
+        if A == [] :            # A가 공집합일 경우 B의 승리
             print('B')
             break
-        if B == [] :
+        if B == [] :            # B가 공집합일 경우 B의 승리
             print('A')
             break
-        if max(A) > max(B) :
+        if max(A) > max(B) :            # A의 최대값이 B의 최대값보다 클 경우 A의 승리
             print('A')
             break
-        elif max(A) < max(B) :
+        elif max(A) < max(B) :            # B의 최대값이 A의 최대값보다 클 경우 B의 승리
             print('B')
             break
         else :
-            if A.count(max(A)) > B.count(max(B)) :
-                print('A')
+            if A.count(max(A)) > B.count(max(B)) :    # A의 최대값과 B의 최대값이 동일할 때,
+                print('A')                            # A의 수가 더 많으면 A의 승리
                 break
-            elif A.count(max(A)) < B.count(max(B)) :
-                print('B')
+            elif A.count(max(A)) < B.count(max(B)) :    # A의 최대값과 B의 최대값이 동일할 때,
+                print('B')                              # B의 수가 더 많으면 A의 승리
                 break
-            else :
-                for i in range(A.count(max(A))) :
+            else :                                      # A의 최대값과 B의 최대값이 동일할 때,
+                for i in range(A.count(max(A))) :       # 둘의 개수도 동일하다면, 최대값 모두 삭제
                     A.remove(max(A))
                     B.remove(max(B))
 
